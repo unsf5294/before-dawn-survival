03aee67fe07393d4dc5243386a2ff80a13dce8ff
EthannnLai <140771714+EthannnLai@users.noreply.github.com>
2023-10-05 20:06:54 +1100

    UI scale change

--- Assets/Scripts/MonsterSpawner.cs
+++ Assets/Scripts/MonsterSpawner.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class MonsterSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject monsterPrefab; 
-    [SerializeField] private Transform player; // Add this reference to the player's transform
+    [SerializeField] private Transform player; 
     [SerializeField] private float spawnInterval = 10.0f; 
-    private float spawnRadius = 10.0f; // The radius around the spawner where monsters can spawn
+    private float spawnRadius = 10.0f; 
 
     private void Start()
     {
-        // Start the spawn loop
+        
         InvokeRepeating("SpawnMonster", 0f, spawnInterval);
     }
 
@@ -19,14 +19,14 @@ public class MonsterSpawner : MonoBehaviour
     {
         if (monsterPrefab && player)
         {
-            // Random position generation around the spawner within the defined radius
+
             Vector3 randomOffset = Random.insideUnitSphere * spawnRadius;
             randomOffset.y = 0; // Keep them at the same vertical level
             Vector3 spawnPosition = transform.position + randomOffset;
 
             GameObject spawnedMonster = Instantiate(monsterPrefab, spawnPosition, Quaternion.identity);
 
-            // Set the player reference for the MonsterMovement script on the newly spawned monster
+
             MonsterMovement monsterMovement = spawnedMonster.GetComponent<MonsterMovement>();
             if (monsterMovement)
             {
