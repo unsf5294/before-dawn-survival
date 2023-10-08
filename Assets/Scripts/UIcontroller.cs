3e368b558a7fa3324209c0571d6f7d4a33794892
EthannnLai <140771714+EthannnLai@users.noreply.github.com>
2023-10-08 12:09:35 +1100

    Operation Instructions

--- Assets/Scripts/UIcontroller.cs
+++ Assets/Scripts/UIcontroller.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
@@ -8,11 +10,15 @@ public class UIManager : MonoBehaviour
     public Button settingButton;
     public Canvas menuUI;
     public Canvas combatUI;
+    public TMP_Text infoText;
+
+    private bool hasShownInfoText = false;  
 
     private void Start()
     {
         menuUI.enabled = true;
         combatUI.enabled = false;
+        infoText.enabled = false;  
     }
 
     private void Update()
@@ -20,7 +26,6 @@ public class UIManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.J))
         {
             StartGame();
-
         }
 
         if (Input.GetKeyDown(KeyCode.K))
@@ -33,11 +38,41 @@ public class UIManager : MonoBehaviour
     {
         menuUI.enabled = false;
         combatUI.enabled = true;
+
+        if (!hasShownInfoText)
+        {
+            StartCoroutine(ShowAndHideInfoText());
+            hasShownInfoText = true;  
+        }
+
         //SceneManager.LoadScene("StartScene");
     }
 
     public void OpenSettings()
     {
         // SceneManager.LoadScene("SettingScene");
+    }
+
+    IEnumerator ShowAndHideInfoText()
+    {
+        string[] messages = {
+            "press 'j' 'k' to attack",
+            "kill monsters to heal",
+            "find artifacts to regain faith"
+        };
+
+        foreach (string message in messages)
+        {
+            infoText.text = message;
+            infoText.enabled = true;
+
+            // Wait for 5 seconds
+            yield return new WaitForSeconds(5f);
+
+            infoText.enabled = false;
+
+            // If you want a gap between hiding one message and showing the next, uncomment the line below:
+            // yield return new WaitForSeconds(1f);
+        }
     }
 }
